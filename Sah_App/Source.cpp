23d06a01cdfb0be7528f601e764d46f9804a24bc
RealMeKaNiK <dautovic2000@gmail.com>
2021-05-18 20:57:18 +0200

    First ever commit

---
+++ Sah_App/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1200, 800), "Hello, SFML world!");
+	window.setFramerateLimit(60);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.display();
+	}
+	return 0;
+}
